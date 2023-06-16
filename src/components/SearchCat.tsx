fa61f4ae2c5eb9ae4405732b7e584268b953b311
CryingSadBaby <94829108+CryingSadBaby@users.noreply.github.com>
2023-06-16 08:33:37 +0000

    fixed search cat function to handle boolean and integer

--- src/components/SearchCat.tsx
+++ src/components/SearchCat.tsx
@@ -6,15 +6,37 @@ import {LoadingOutlined} from '@ant-design/icons'
 import {api} from '../resources/myapi'
 import {status, json} from '../resources/requestHandlers'
 
+
+const inputRule = [
+  {required: true, message:'Please input keywords'}
+]
+
 function SearchCat(){
   const [searching, setSearching] = React.useState(false)
   const [searchComp, setSearchComp] = React.useState(false)
   const [cats, setCat] = React.useState(null)
   const [loading, setLoading] = React.useState(true)
+  const [input, setInput] = React.useState('text')
+  const [error, setError] = React.useState(false)
+
+  const [form] = Form.useForm()
+
+  const change = (value) => {
+    if(value==="age"){
+      setInput('number')
+    } else if (value==="gender") {
+      setInput('gender')
+    } else if (value==="neutered"){
+      setInput('neutered')
+    } else {
+      setInput('text')
+    }
+  }
   
   const search = (values) => {
     const data = {[values.key]:values.value}
     setSearching(true)
+    setError(false)
     fetch(`${api.uri}/pets/search`,{
       method: "POST",
       headers: {
@@ -32,7 +54,8 @@ function SearchCat(){
       setSearchComp(true)
     })
     .catch(err=>{
-      console.log(err)
+      setError(true)
+      setSearching(false)
     })
   }
 
@@ -42,52 +65,68 @@ function SearchCat(){
         const antIcon = <LoadingOutlined style={{fontSize: 48}} spin />
         return(<Spin indicator={antIcon}/>)
       }else{
-        return(
-          <Row>
-            {
-              cats&&cats.map((
-                {id,petname,des,breed,age,gender,neutered,imageurl}
-              )=>(
-                <Col span={8}>
-                  <Card 
-                    key={id} 
-                    cover={<img src={imageurl} alt={id}/>}
-                    style={{width: 300, color:'purple'}}
-                    hoverable>
-                    <h3>{petname}</h3>
-                    <p>{des}</p>
-                    <p>Breed: {breed}</p>
-                    <p>Age: {age}</p>
-                    {!gender&&<p>Gender: female</p>}
-                    {gender&&<p>Gender: male</p>}
-                    {!neutered&&<p>Neutered: No</p>}
-                    {neutered&&<p>Neutered: Yes</p>}
-                    <Link to={ `/cat/${id}` }>Details</Link>
-                  </Card>
-                </Col>
-              ))
-            }
-          </Row>
-        )
+        if(!error){
+          return(
+            <Row>
+              {
+                cats&&cats.map((
+                  {id,petname,des,breed,age,gender,neutered,imageurl}
+                )=>(
+                  <Col span={8}>
+                    <Card 
+                      key={id} 
+                      cover={<img src={imageurl} alt={id}/>}
+                      style={{width: 300, color:'purple'}}
+                      hoverable>
+                      <h3>{petname}</h3>
+                      <p>{des}</p>
+                      <p>Breed: {breed}</p>
+                      <p>Age: {age}</p>
+                      {!gender&&<p>Gender: female</p>}
+                      {gender&&<p>Gender: male</p>}
+                      {!neutered&&<p>Neutered: No</p>}
+                      {neutered&&<p>Neutered: Yes</p>}
+                      <Link to={ `/cat/${id}` }>Details</Link>
+                    </Card>
+                  </Col>
+                ))
+              }
+            </Row>
+          )
+        } else {
+          return(<h1>There is no result</h1>)
+        }
       }
     }
   }
 
-  
-
   return(
     <main>
-      <Form onFinish={search}>
+      <Form form={form} onFinish={search}>
         <Form.Item name="key" label="Search by" initialValue='petname'>
           <Select options={
           [
             {value:'petname', label: 'Name'},
             {value:'breed', label: 'Breed'},
+            {value:'gender', label: 'Gender'},
+            {value:'neutered', label: 'Neutered State'},
             {value:'age', label: 'Age'}
-          ]} />
+          ]} onChange={change} />
         </Form.Item>
-        <Form.Item name="value" label="Keywords" initialValue='*'>
-          <Input/>
+        <Form.Item name="value" label="Keywords" rules={inputRule}>
+          {input==="text"?(<Input />):
+          input==="number"?(<InputNumber/>):
+          input==="gender"?(
+            <Select 
+              options={
+              [{value: false, label: 'female'},
+               {value: true, label: 'male'}]}/>):
+          input==="neutered"?(
+            <Select 
+              options={
+              [{value: false, label: 'No'},
+               {value: true, label: 'Yes'}]}/>
+          ):null}
         </Form.Item>
         <Form.Item>
           {!searching&&<Button type="primary" htmlType="submit">Search</Button>}
